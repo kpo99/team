2847c71268d22fa8077385556ccb92455c2c04cb
kpo99 <krivenkopetr99@gmail.com>
2016-04-07 16:59:35 +0300

    fix

--- sprint2/Application/Application/Исходный код.cpp
+++ sprint2/Application/Application/Исходный код.cpp
@@ -2,85 +2,132 @@
 #include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
+#define MAX_SPEED 200
+
 using namespace sf;
+
 int main()
 {
-	RenderWindow window(VideoMode(900, 675), "App");
+	RenderWindow window(VideoMode(900, 675), "BotiCar");
+	int status = 0;
 
-	Image fon;
+	Image fon; 
 	fon.loadFromFile("images/background.png");
-
-	Texture fontexture;
-	fontexture.loadFromImage(fon);
-
-	Sprite fonsprite;
-	fonsprite.setTexture(fontexture);
-	fonsprite.setPosition(0, 0);
-
 	Image left;
 	left.loadFromFile("images/left.png");
-	left.createMaskFromColor(Color(255,255,255));
-
-	Texture lefttexture;
-	lefttexture.loadFromImage(left);
-
-	Sprite leftsprite;
-	leftsprite.setTexture(lefttexture);
-
 	Image right;
 	right.loadFromFile("images/right.png");
-	right.createMaskFromColor(Color(255, 255, 255));
-
-	Texture righttexture;
-	righttexture.loadFromImage(right);
-
-	Sprite rightsprite;
-	rightsprite.setTexture(righttexture);
-
 	Image led;
 	led.loadFromFile("images/led.png");
-	led.createMaskFromColor(Color(255, 255, 255));
-
-	Texture ledtexture;
+	Texture fontexture, lefttexture, righttexture, ledtexture;
+	fontexture.loadFromImage(fon);
+	lefttexture.loadFromImage(left);
+	righttexture.loadFromImage(right);
 	ledtexture.loadFromImage(led);
 
-	Sprite ledsprite;
+	Sprite fonsprite, leftsprite, rightsprite, ledsprite;
+	fonsprite.setTexture(fontexture);
+	leftsprite.setTexture(lefttexture);
+	rightsprite.setTexture(righttexture);
 	ledsprite.setTexture(ledtexture);
-	int status = 0;
+
 	while (window.isOpen())
 	{
-
 		Event event;
 		while (window.pollEvent(event))
 		{
-
 			if (event.type == Event::Closed)
 				window.close();
 		}
-	if (Keyboard::isKeyPressed(Keyboard::A)) {
-			for (int i = 0; i < 1500; i++){
+		if (Keyboard::isKeyPressed(Keyboard::A))
+		{
+			if (status != 1)
+			{
 				window.draw(leftsprite);
 				window.display();
-			}
-			for (int i = 0; i < 1500; i++){
+				Sleep(150);
+
 				window.draw(fonsprite);
 				window.display();
+				Sleep(150);
+			}
+			else
+			{
+				window.draw(leftsprite);
+				window.display();
+				Sleep(150);
+
+				window.draw(ledsprite);
+				window.display();
+				Sleep(150);
 			}
 		}
-		else if (Keyboard::isKeyPressed(Keyboard::D)){
-			for (int i = 0; i < 100; i++){
+		else if (Keyboard::isKeyPressed(Keyboard::D))
+		{
+			if (status != 1)
+			{
 				window.draw(rightsprite);
 				window.display();
+				Sleep(150);
+
+				window.draw(fonsprite);
+				window.display();
+				Sleep(150);
+			}
+			else
+			{
+				window.draw(rightsprite);
+				window.display();
+				Sleep(150);
+
+				window.draw(ledsprite);
+				window.display();
+				Sleep(150);
 			}
 		}
-		else if (Keyboard::isKeyPressed(Keyboard::L)){
+		else if (Keyboard::isKeyPressed(Keyboard::L))
+		{
 			status++;
 			status = status % 2;
-	
-				}
+			Sleep(150);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::W))
+		{
 
-		window.draw(fonsprite);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::S))
+		{
+
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Num1))
+		{
+
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Num2))
+		{
+
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Num3))
+		{
+
+		}
+		else
+		{
+
+		}
+
+		if (status == 1)
+		{
+			window.draw(ledsprite);
+		}
+		else
+		{
+			window.draw(fonsprite);
+		}
 		window.display();
 	}
+
 	return 0;
 }
